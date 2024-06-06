6e7c08c04d0ac1e4517f091bdb95418eb71bedeb
Sam Hemond <samhemond@comcast.net>
2024-06-05 23:30:20 -0400

    Spellcheck
    Spellcheck

--- CS360ArtifactSamuelHemond/app/src/main/java/com/example/cs360project3samuelhemond/EncryptionAlgorithm.java
+++ CS360ArtifactSamuelHemond/app/src/main/java/com/example/cs360project3samuelhemond/EncryptionAlgorithm.java
@@ -40,10 +40,10 @@ public class EncryptionAlgorithm {
             0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
     };
 
-    //varible for bytes being worked on
+    //variable for bytes being worked on
     byte[] currentBytes;
 
-    //varible for 16 32 bit words and 48 additional words for processing
+    //variable for 16 32 bit words and 48 additional words for processing
     int[] words;
 
 
